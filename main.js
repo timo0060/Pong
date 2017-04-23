a8b25ea59952818a4f051814c427930530adc08c
Timothy Radder <timo0060@gmail.com>
2017-04-23 00:08:39 -0400

    Add Basic files and give Canvas black background

---
+++ main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Tim on 4/22/2017.
+ */
+var c, ctx;
+
+var WIDTH = 800, HEIGHT = 600;
+
+window.onload = function(){
+    init();
+};
+
+function init(){
+   c = document.getElementById('canvas');
+   ctx = c.getContext('2d');
+
+   //set width and height
+};
