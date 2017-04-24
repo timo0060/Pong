067412a134cbde758409c1624e09e832b2b49e50
Timothy Radder <timo0060@gmail.com>
2017-04-23 23:07:57 -0400

    Add ball and paddle collision. TODO: Fix sticky collision on paddle

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * Created by Tim on 4/22/2017.
  */
 var c, ctx;
-
+var player, ball;
 var WIDTH = 1000, HEIGHT = 700;
 
 window.onload = function(){
@@ -20,11 +20,11 @@ function init(){
     ctx.fillStyle = "#000000";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-    var ball = new Ball();
-    var player = new Player(25, HEIGHT/4);
-
+    ball = new Ball(WIDTH, HEIGHT);
+    player = new Player(WIDTH, HEIGHT, "Player");
+    console.log(player);
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(10, HEIGHT/4 + (player.height/2), player.width, player.height);
+    ctx.fillRect(player.x, player.y, player.width, player.height);
 
     ctx.fillRect((WIDTH/2 - ball.width), (HEIGHT/2 - ball.height), ball.width, ball.height);
 
@@ -33,16 +33,18 @@ function init(){
 
 function loop(){
    Clear();
-
+   ball.Move();
+   ball.CheckBoundries(WIDTH, HEIGHT);
+   ball.CheckPaddleCollision(player);
    Draw();
 };
 
 function Draw(){
     ctx.fillStyle = "#FFFFFF";
     //Draw Player
-    ctx.fillRect(10, HEIGHT/4 + (player.height/2), player.width, player.height);
+    ctx.fillRect(player.x, player.y, player.width, player.height);
     //Draw Ball
-    ctx.fillRect((WIDTH/2 - ball.width), (HEIGHT/2 - ball.height), ball.width, ball.height);
+    ctx.fillRect(ball.x, ball.y, ball.width, ball.height);
 };
 
 function Clear(){
